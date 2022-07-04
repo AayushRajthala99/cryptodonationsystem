9580a4ef49cb417951da883ee6fb24ee5a973a66
Aayush Rajthala <Aayushrajthala.075@kathford.edu.np>
2022-07-04 22:01:31 +0545

    Modified Migration

--- migrations/transaction/1-create-transaction-table.sql
+++ migrations/transaction/1-create-transaction-table.sql
@@ -3,12 +3,19 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
+-- Generation Time: Jul 04, 2022 at 06:14 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `cryptodonationsystem`
@@ -17,30 +24,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaction`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `transaction` (
+CREATE TABLE `user` (
   `id` int(11) UNSIGNED NOT NULL,
-  `user_id` int(11) UNSIGNED NOT NULL,
-  `recipientwalletid` varchar(45) DEFAULT NULL,
-  `amount` varchar(255) NOT NULL,
-  `status` tinyint(1) NOT NULL COMMENT '1 = "Success", 0 = "Failed"',
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `walletid` varchar(45) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Indexes for table `transaction`
+-- Indexes for dumped tables
 --
-ALTER TABLE `transaction`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_user_id` (`user_id`);
 
 --
--- Constraints for table `transaction`
+-- Indexes for table `user`
 --
-ALTER TABLE `transaction`
-  ADD CONSTRAINT `fk0_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE;
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email_UNIQUE` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
