5d206a9cc7111d73702692964dde8704dc045c0f
SubhamShrestha <subhamshrestha.075@kathford.edu.np>
2022-07-16 11:54:36 +0545

    Fixed login backend validation and renamed DashboardController

--- src/middleware/login.js
+++ src/middleware/login.js
@@ -1,11 +1,10 @@
 const yup = require("yup");
+const { getColumnInfo } = require("../utils/utils");
 
 let inputLength = {
   min: 3,
   max: 60,
 };
-
-const { getColumnInfo } = require("../models/Login.model");
 
 //Validation Schema for login...
 const linkSchemaLogin = yup.object({
@@ -15,11 +14,7 @@ const linkSchemaLogin = yup.object({
       .min(inputLength.min, "* INVALID VALUE LENGTH!")
       .max(inputLength.max, "* INVALID VALUE LENGTH!")
       .required("* EMAIL REQUIRED!")
-      .matches(
-        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
-        "* INVALID FORMAT!"
-      )
-      .test("* User doesn't exit", "* User doesn't exit", () => {
+      .test("* User doesn't Exist", "* User doesn't Exist", () => {
         return userArray.result.length !== 0;
       }),
 
@@ -41,7 +36,7 @@ const validateLogin = (schema) => async (req, res, next) => {
       password: password,
     };
 
-    userArray = await getColumnInfo("email", email);
+    userArray = await getColumnInfo("user", "email", "email", email);
     if (userArray.status) {
       try {
         await schema.validate(
@@ -58,7 +53,7 @@ const validateLogin = (schema) => async (req, res, next) => {
           email: null,
           password: null,
         };
-          
+
         // Storing error message...
         error.inner.forEach((e) => {
           if (e.path.slice(5) == "email") {
@@ -67,14 +62,12 @@ const validateLogin = (schema) => async (req, res, next) => {
             errorMessage.password = e.errors[0];
           }
         });
-        console.log(error.inner)
+        console.log(error.inner);
         res.render("login", {
           result: result,
           errorMessage: errorMessage,
         });
       }
-    } else {
-      res.render("error");
     }
   } catch (error) {
     throw error;
--- src/models/Login.model.js
+++ src/models/Login.model.js
@@ -1,17 +1,7 @@
 const db = require("../../config/mysql");
 const { logger } = require("../utils/logger");
 
-const promisifiedQuery = (options) => {
-  return new Promise((resolve, reject) => {
-    db.query(options, function (error, result) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(result);
-      }
-    });
-  });
-};
+const { promisifiedQuery } = require("../utils/utils");
 
 const getLoginInfo = async (email) => {
   try {
@@ -30,25 +20,7 @@ const getLoginInfo = async (email) => {
     };
   }
 };
-const getColumnInfo = async (column, email) => {
-  try {
-    const result = await promisifiedQuery(
-      `SELECT ${column} FROM user where email='${email}' AND deleted_at is NULL;`
-    );
-    return {
-      status: true,
-      result: result.map((value) => value[column]),
-    };
-  } catch (error) {
-    logger.error(`Column Info Fetch Error:  ${error}`);
-    return {
-      status: false,
-      error: error,
-    };
-  }
-};
 
 module.exports = {
   getLoginInfo,
-  getColumnInfo,
 };
